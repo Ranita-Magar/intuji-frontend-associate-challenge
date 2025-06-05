24b285371681c7af66ff5f047d81171bdc92f8d8
Ranita-Magar <ranitamagar10@gmail.com>
2025-06-05 10:05:00 +0545

    making responsive

---
+++ script.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const dashboardItem = document.getElementById("dashboard");
+  if (dashboardItem) {
+    dashboardItem.classList.add("active");
+  }
+});
